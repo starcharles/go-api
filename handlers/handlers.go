f6f6b51750a6f44d833d42fbaa1b63d4e95aeefa
naoto <stardustforever012@gmail.com>
2022-10-23 09:43:58 +0900

    Handlerにメソッドチェックを追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,27 +7,53 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+		return
+	}
+
+	afterAll(w)
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article.... \n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article.... \n")
+	}
+
+	afterAll(w)
 }
 
 func ListArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List \n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List \n")
+	}
+
+	afterAll(w)
 }
 
 func FindArticleHandler(w http.ResponseWriter, req *http.Request) {
-	articleId := 1
-	res := fmt.Sprintf("Article No.%d\n", articleId)
-	io.WriteString(w, res)
+	if req.Method == http.MethodGet {
+		articleId := 1
+		res := fmt.Sprintf("Article No.%d\n", articleId)
+		io.WriteString(w, res)
+	}
+	afterAll(w)
 }
 
 func PostNiceArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice... \n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice... \n")
+	}
+	afterAll(w)
 }
 
 func PostCommnetHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment... \n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment... \n")
+	}
+	afterAll(w)
+}
+
+func afterAll(w http.ResponseWriter) {
+	http.Error(w, "Invalid http method", http.StatusMethodNotAllowed)
 }
